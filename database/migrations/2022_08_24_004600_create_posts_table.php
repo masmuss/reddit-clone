c1f8e6c98186d61636b532a6737e505b11dd4aca
A. Musafir Khoirul F <ahmusafir@it.student.pens.ac.id>
2022-08-24 08:10:13 +0700

    add community, post, comment, post vote models and migrations

---
+++ database/migrations/2022_08_24_004600_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('posts', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('community_id')->references('id')->on('communities')->onDelete('cascade');
+			$table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+			$table->string('title');
+			$table->string('slug');
+			$table->text('url');
+			$table->text('description');
+			$table->softDeletes();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('posts');
+	}
+};
